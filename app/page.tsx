820a336675f0d5752112c6415fb5a4c3bcbaa7c2
eunhhu <necpition205@gmail.com>
2026-07-28 05:25:22 +0100

    chore: remediate Dependabot vulnerabilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import Head from 'next/head'
-import Main from './main'
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const Main = dynamic(() => import('./main'), { ssr: false })
 
 export default function Home() {
-  return (<Main />)
+  return <Main />
 }
